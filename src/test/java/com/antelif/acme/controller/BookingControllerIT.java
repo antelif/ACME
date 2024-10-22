0c588684b8d804d42bd4c3be0608968427696d87
ntelinasia <ana.ntelif@gmail.com>
2024-10-22 15:31:09 +0300

    Add tests

--- src/test/java/com/antelif/acme/controller/BookingControllerIT.java
+++ src/test/java/com/antelif/acme/controller/BookingControllerIT.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -12,10 +13,14 @@ import com.antelif.acme.config.IntegrationTestBase;
 import com.antelif.acme.model.error.ErrorResponse;
 import com.antelif.acme.model.request.BookingRequest;
 import com.antelif.acme.model.request.MeetingRoomRequest;
-import com.antelif.acme.model.response.BookingResponse;
 import com.antelif.acme.model.response.MeetingRoomResponse;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -62,14 +67,14 @@ class BookingControllerIT extends IntegrationTestBase {
     this.bookingRequest.setMeetingRoomId(meetingRoomId);
 
     String bookingResponseAsString = postBooking(bookingRequest);
-    BookingResponse response =
-        objectMapper.readValue(bookingResponseAsString, BookingResponse.class);
+    Map<String, String> response =
+        objectMapper.readValue(bookingResponseAsString, new TypeReference<>() {});
 
-    assertNotNull(response.getId());
-    assertEquals(meetingRoomId, response.getMeetingRoomId());
-    assertEquals("something@mail.com", response.getEmployeeEmail());
-    assertEquals(fromDate, response.getFromDate());
-    assertEquals(toDate, response.getToDate());
+    assertNotNull(response.get("id"));
+    assertEquals(meetingRoomId.toString(), response.get("meetingRoomId"));
+    assertEquals("something@mail.com", response.get("employeeEmail"));
+    assertNotNull(response.get("fromDate"));
+    assertNotNull(response.get("toDate"));
   }
 
   @Test
@@ -91,7 +96,9 @@ class BookingControllerIT extends IntegrationTestBase {
     this.bookingRequest.setMeetingRoomId(meetingRoomId);
 
     String bookingResponseAsString = postBooking(bookingRequest);
-    Long bookingId = objectMapper.readValue(bookingResponseAsString, BookingResponse.class).getId();
+    String idAsString =
+        objectMapper.readValue(bookingResponseAsString, Map.class).get("id").toString();
+    Long bookingId = Long.parseLong(idAsString);
 
     assertNotNull(bookingId);
 
@@ -112,6 +119,115 @@ class BookingControllerIT extends IntegrationTestBase {
         objectMapper.readValue(errorResponseAsString, ErrorResponse.class);
 
     assertEquals(ENTITY_DOES_NOT_EXIST_ERROR.name(), errorResponse.getName());
+  }
+
+  @Test
+  @DisplayName("Booking: Successfully get booking by room and date")
+  @SneakyThrows
+  void testBookingIsRetrievedByNameAndDate() {
+    // Create meeting room
+    this.meetingRoomRequest.setName("testBookingIsRetrievedByNameAndDate");
+    String meetingRoomResponseAsString = postMeetingRoom(meetingRoomRequest);
+    MeetingRoomResponse meetingRoom =
+        objectMapper.readValue(meetingRoomResponseAsString, MeetingRoomResponse.class);
+
+    // Create booking for the above meeting room id
+    Instant fromDate = Instant.now().plus(1, ChronoUnit.DAYS);
+    Instant toDate = fromDate.plus(1, ChronoUnit.HOURS);
+    this.bookingRequest.setFromDate(fromDate);
+    this.bookingRequest.setToDate(toDate);
+    this.bookingRequest.setEmployeeEmail("something@mail.com");
+    this.bookingRequest.setMeetingRoomId(meetingRoom.getId());
+
+    postBooking(bookingRequest);
+
+    // Request booking for the above meeting room
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    String dateParam = format.format(Date.from(fromDate));
+
+    String allBookingsAsString = getBooking(meetingRoom.getName(), dateParam);
+    Map bookings = objectMapper.readValue(allBookingsAsString, Map.class);
+    List actualContent =
+        objectMapper.readValue(
+            objectMapper.writeValueAsString(bookings.get("content")), List.class);
+
+    assertEquals(1, actualContent.size());
+  }
+
+  @Test
+  @DisplayName("Booking: Booking is not retrieved with wrong room name")
+  @SneakyThrows
+  void testBookingIsNotRetrievedWithWrongRoomName() {
+    // Create meeting room
+    this.meetingRoomRequest.setName("testBookingIsNotRetrievedWithWrongRoomName");
+    String meetingRoomResponseAsString = postMeetingRoom(meetingRoomRequest);
+    MeetingRoomResponse meetingRoom =
+        objectMapper.readValue(meetingRoomResponseAsString, MeetingRoomResponse.class);
+
+    // Create booking for the above meeting room id
+    Instant fromDate = Instant.now().plus(1, ChronoUnit.DAYS);
+    Instant toDate = fromDate.plus(1, ChronoUnit.HOURS);
+    this.bookingRequest.setFromDate(fromDate);
+    this.bookingRequest.setToDate(toDate);
+    this.bookingRequest.setEmployeeEmail("something@mail.com");
+    this.bookingRequest.setMeetingRoomId(meetingRoom.getId());
+
+    postBooking(bookingRequest);
+
+    // Request booking for a meeting room that does not exist
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    String dateParam = format.format(Date.from(fromDate));
+
+    String allBookingsAsString = getBooking("THIS_MEETING_ROOM_NAME_DOES_NOT_EXIST", dateParam);
+    Map bookings = objectMapper.readValue(allBookingsAsString, Map.class);
+    List actualContent =
+        objectMapper.readValue(
+            objectMapper.writeValueAsString(bookings.get("content")), List.class);
+
+    assertEquals(0, actualContent.size());
+  }
+
+  @Test
+  @DisplayName("Booking: Booking is not retrieved with wrong dates")
+  @SneakyThrows
+  void testBookingIsNotRetrievedWithWrongDates() {
+    // Create meeting room
+    this.meetingRoomRequest.setName("testBookingIsNotRetrievedWithWrongDates");
+    String meetingRoomResponseAsString = postMeetingRoom(meetingRoomRequest);
+    MeetingRoomResponse meetingRoom =
+        objectMapper.readValue(meetingRoomResponseAsString, MeetingRoomResponse.class);
+
+    // Create booking for the above meeting room id
+    Instant fromDate = Instant.now().plus(1, ChronoUnit.DAYS);
+    Instant toDate = fromDate.plus(1, ChronoUnit.HOURS);
+    this.bookingRequest.setFromDate(fromDate);
+    this.bookingRequest.setToDate(toDate);
+    this.bookingRequest.setEmployeeEmail("something@mail.com");
+    this.bookingRequest.setMeetingRoomId(meetingRoom.getId());
+
+    postBooking(bookingRequest);
+
+    // Request booking for a meeting room that does not exist
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    String dateParam = format.format(Date.from(fromDate.plus(10, ChronoUnit.DAYS)));
+
+    String allBookingsAsString = getBooking(meetingRoom.getName(), dateParam);
+    Map bookings = objectMapper.readValue(allBookingsAsString, Map.class);
+    List actualContent =
+        objectMapper.readValue(
+            objectMapper.writeValueAsString(bookings.get("content")), List.class);
+
+    assertEquals(0, actualContent.size());
+  }
+
+  @SneakyThrows
+  private String getBooking(String room, String date) {
+    return this.mockMvc
+        .perform(get(String.format("/acme/booking?room=%s&date=%s", room, date)))
+        .andDo(print())
+        .andReturn()
+        .getResponse()
+        .getContentAsString();
   }
 
   @SneakyThrows
