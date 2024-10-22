a584ded580d57683ef07a017d31ff5ecc4027d7e
Anastasia Ntelifilippidi <59694309+antelif@users.noreply.github.com>
2024-10-22 15:35:36 +0300

    Mvp/get bookings (#6)
    * Add get bookings controller implementation
    
    * Add tests

--- src/main/java/com/antelif/acme/repository/BookingRepository.java
+++ src/main/java/com/antelif/acme/repository/BookingRepository.java
@@ -1,9 +1,15 @@
 package com.antelif.acme.repository;
 
 import com.antelif.acme.model.entity.Booking;
+import java.time.Instant;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /** Booking repository. */
 @Repository
-public interface BookingRepository extends JpaRepository<Booking, Long> {}
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+  List<Booking> findByMeetingRoomNameAndFromDateAfter(
+      String meetingRoomName, Instant fromDate, Pageable pageable);
+}
